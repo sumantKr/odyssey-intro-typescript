fe7b3c4098a81b99ee1f44dd61e2621a1f98fc54
sumantKr <52274791+sumantKr@users.noreply.github.com>
2024-08-07 01:06:56 +0530

    final

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import { type CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+    schema: './src/schema.graphql',
+    generates: {
+        './src/types.ts': {
+            plugins: ["typescript", "typescript-resolvers"],
+            config:{
+                contextType:'./context#DataSourceContext'
+            }
+        }
+    },
+    
+};
+
+export default config;
